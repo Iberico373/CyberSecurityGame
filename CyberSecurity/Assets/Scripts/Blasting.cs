523568459a9111cc8b15bd15b654705f20c9329e
Iberico373 <julianarthadjaya.id@gmail.com>
2022-08-22 20:49:01 +0800

    Revert "yes5"
    This reverts commit 7a7eb5ce84c1c81be73d6410a050e2727085fbe5.

--- CyberSecurity/Assets/Scripts/Blasting.cs
+++ CyberSecurity/Assets/Scripts/Blasting.cs
@@ -6,18 +6,15 @@ public class Blasting : MonoBehaviour
 {
     public Blast target;
     public GameObject strike;
-    public GameObject waves;
     UnitManager manager;
     public void Blastin()
     {
         manager = UnitManager.instance;
         strike = target.target;
         Vector3 dir = (strike.transform.position - manager.selectedCharacter.transform.position).normalized;
-        GameObject summonedWave = Instantiate(waves,transform);
-        summonedWave.transform.position = transform.position + new Vector3(0,3,0) + transform.forward * 3;
-
         strike.transform.position = strike.transform.position + dir * manager.grid.nodeRadius * 2;
         strike.GetComponent<BaseAI>().aggrolist.Insert(0, manager.selectedCharacter.gameObject);
         manager.grid.UpdateGrid();
     }
+    
 }
