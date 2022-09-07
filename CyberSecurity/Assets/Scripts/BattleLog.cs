da4d75865ded0fbfddfaf61ce9588c8bdc6c8808
Iberico373 <julianarthadjaya.id@gmail.com>
2022-09-07 11:24:34 +0800

    test

--- CyberSecurity/Assets/Scripts/BattleLog.cs
+++ CyberSecurity/Assets/Scripts/BattleLog.cs
@@ -8,13 +8,10 @@ public class BattleLog : MonoBehaviour
     public TMPro.TextMeshProUGUI battleText;
 
     bool isActive;
-    float delay;
-    public float time;
-    bool countdown;
+
     private void Start()
     {
         isActive = false;
-        delay = time;
     }
 
     public void UpdateBattleLog(string characterName, string action, string targetName = "")
@@ -22,31 +19,8 @@ public class BattleLog : MonoBehaviour
        if (!isActive)
         {
             battleLog.SetActive(true);
-            isActive = true;
         }
-        delay = time;
+
         battleText.text = characterName + action + targetName;
-    }
-
-    private void Update()
-    {
-        if(isActive)
-        {
-            delay -= 1;
-            countdown = true;
-        }
-        if(delay <= 0 && countdown)
-        {
-            isActive = false;
-            battleLog.SetActive(false);
-            countdown = false;
-        }
-    }
-
-    IEnumerator DelayedDeactivate()
-    {
-        yield return new WaitForSeconds(2);
-        battleLog.SetActive(false);
-        isActive = false;
     }
 }
